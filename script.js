aba8e2b68babb38d767eddcfc986b26b3e8cc9dd
Francisco Solano <fsolanosilva@yahoo.com.br>
2023-11-15 09:31:22 -0300

    melhoria do grid

--- script.js
+++ script.js
@@ -48,6 +48,10 @@ document.addEventListener('DOMContentLoaded', function () {
         return optionsCell;
     }
 
+    // script.js
+
+    // ... (seu código anterior)
+
     function displayData() {
         const startIndex = (currentPage - 1) * itemsPerPage;
         const endIndex = startIndex + itemsPerPage;
@@ -57,21 +61,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
         currentData.forEach(item => {
             const row = document.createElement('tr');
-
-            // Adicionar colunas de dados
-            Object.keys(item).forEach(key => {
-                const cell = document.createElement('td');
-                cell.textContent = item[key];
-                row.appendChild(cell);
-            });
-
-            // Adicionar coluna "Options" com botões de edição e exclusão
-            row.appendChild(createOptionsButtons());
-
-            // Adicionar a linha à tabela
+            row.innerHTML = `
+            <td>${item.id}</td>
+            <td>${item.name}</td>
+            <td>${item.email}</td>
+            <td class="options-column">
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-warning btn-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                            <path d="M11.742 0.742a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-10 10a1 1 0 0 1-0.446 0.279l-4-1a1 1 0 0 1-.554-1.326l1-4a1 1 0 0 1 0.819-0.748l13-3a1 1 0 0 1 1.195 1.195l-3 13a1 1 0 0 1-0.748 0.819l-4 1a1 1 0 0 1-1.326-0.554l-1-4a1 1 0 0 1 0.279-0.446z"/>
+                        </svg>
+                    </button>
+                    <button type="button" class="btn btn-danger btn-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M3 1a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v1H3V1zm10 2H0v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zm-1 11a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4h9v10z"/>
+                            <path d="M5 7a1 1 0 0 1 1-1h1a1 1 0 0 1 0 2H6a1 1 0 0 1-1-1zm0 4a1 1 0 0 1 1-1h1a1 1 0 0 1 0 2H6a1 1 0 0 1-1-1z"/>
+                        </svg>
+                    </button>
+                    <!-- Adicione mais botões conforme necessário -->
+                </div>
+            </td>
+        `;
             tableBody.appendChild(row);
         });
     }
+
+    // ... (seu código anterior)
+
 
     function sortData(column, order) {
         data.sort((a, b) => {
